082fdc40d445f1acd0513bb335e6b8e940af4d4c
Michelene Chon <michelenechon@gmail.com>
2019-11-09 14:00:03 -0800

    Add console.log whenever we enqueue or dequeue

--- server/js/messageQueue.js
+++ server/js/messageQueue.js
@@ -1,11 +1,12 @@
 const messages = []; // the storage unit for messages
 
 module.exports.enqueue = (message) => {
-  console.log(`Enqueing message: ${message}`);
+  console.log(`MQ: Enqueing message: ${message}`);
   messages.push(message);
 };
 
 module.exports.dequeue = () => {
+  console.log(`MQ: Dequeing message`);
   // returns undefined if messages array is empty
   return messages.shift();
 };
