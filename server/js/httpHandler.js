01dc2ef4349a0945eae82d2cfb4ba610123165bb
Michelene Chon <michelenechon@gmail.com>
2019-11-09 14:05:40 -0800

    Successful dequeue invocation from httpHandler

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const messagequeue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
@@ -20,7 +21,11 @@ module.exports.router = (req, res, next = ()=>{}) => {
   let index = Math.floor(Math.random() * 4);
 
   if (req.method === 'GET') {
-    res.write(swimCommand[index]);
+    //res.write(swimCommand[index]);
+    var tryThis = messagequeue.dequeue();
+    console.log(`tryThis = ${tryThis}`);
+    var newCommand = 'down';
+    res.write(newCommand);
   }
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
